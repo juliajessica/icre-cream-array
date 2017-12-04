b96c9a011f4b783cbc075e673e8cc8d2b1e8f959
Jun Li and julia sheremet <julia@>
2017-12-04 11:37:55 -0800

    create webpage with looking a page from an array

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var iceCreams = ["chocolate", "vanilla", "green tea", "peanut butter", "mint"];
+iceCreams.forEach(function(iceCreams) {
+  alert("My favorite Ice cream is " + iceCreams + "!!!!!!");
+});
